cfe8f662ea94854ec1242b08e3e58be01e118111
Abdelhakim baalla <abdelhakimbaalla40@gmail.com>
2024-09-03 14:28:56 +0100

    Starting in day 2 and create challenge 1

---
+++ Day 2/Les Tableaux/challenge 1/challenge1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int tableaux[4] = {1,2,3,4};
+    int i;
+
+    for ( i = 0; i < 4; i++){
+        printf("la valeur: %d \n", tableaux[i]);
+    }
+   return 0;
+}
