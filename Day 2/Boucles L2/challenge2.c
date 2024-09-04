176deb535bb6eccedc4332115f3317bc6300fc63
Abdelhakim Baalla <abdelhakimbaalla40@gmail.com>
2024-09-04 19:26:32 +0100

    changes

--- Day 2/Boucles L2/challenge2.c
+++ Day 2/Boucles L2/challenge2.c
@@ -2,15 +2,19 @@
 #include <stdlib.h>
 
 int main (){
-    int nbr, i;
+    int nbr, i, j;
     char symb = '*';
     printf("Saisez un nombre: ");
     scanf("%d", &nbr);
 
     for (i = 1; i < nbr; i++){
-        for ( int j = 1; j < nbr; j++){
+        for ( j = 1; j <= nbr - i; j++){
+            printf(" ");
+        }
+        for ( j = 1; j <= (2*i-1); j++){
             printf("*");
         }
+     printf("\n");
     }
     
     return 0;
