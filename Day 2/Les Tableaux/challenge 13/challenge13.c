68007a26ed7fdec0f673a4ba2fbb7143fdcf15ad
Abdelhakim baalla <abdelhakimbaalla40@gmail.com>
2024-09-04 15:24:41 +0100

    creating challenge 13 (arrays)

---
+++ Day 2/Les Tableaux/challenge 13/challenge13.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+    int taille,i, valeur;
+    printf("Saisez la taille: ");
+    scanf("%d", &taille);
+    int nombre[taille];
+    for ( i = 0; i < taille; i++){
+        printf("Saisez l elemnt %d: ", i + 1);
+        scanf("%d", &nombre[i]);
+        
+    }
+
+    for ( i = 0; i < taille; i++){
+        if (nombre[i] % 2  != 0){
+            printf("%d", nombre[i]);
+        }
+    }
+    return 0;
+}
