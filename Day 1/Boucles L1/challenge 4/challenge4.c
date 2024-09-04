13ebfd73049adad1991685f7a035e119fd0dd815
Abdelhakim baalla <abdelhakimbaalla40@gmail.com>
2024-09-04 14:35:26 +0100

    Crée challenge 4 en boules

---
+++ Day 1/Boucles L1/challenge 4/challenge4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int nombre, i, operation;
+    printf("Saisez un entier: ");
+    scanf("%d", &nombre);
+
+    operation = 1;
+    for ( i = 0; i < nombre; i++ ){
+        printf("%d, ", operation);
+        operation = operation + 2;
+        
+    }
+
+}
