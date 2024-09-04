fc6a606c16c07fecb5ae3baec4f5028c600a914f
Abdelhakim Baalla <abdelhakimbaalla40@gmail.com>
2024-09-04 16:30:27 +0100

    Add la boucle for

--- Day 1/Boucles L1/challenge 6/challenge6.c
+++ Day 1/Boucles L1/challenge 6/challenge6.c
@@ -2,7 +2,14 @@
 #include <stdlib.h>
 
 int main (){
-    int n;
+    int n, i, result;
+    printf("saisez un entier: ");
+    scanf("%d", &n);
     
+    result = 0;
+    for( i = 0; i < n ; i++ ){
+        result = result + 2;
+        printf("%d ", result);
+    }
     return 0;
 }
