5622ac0a8004e54535dcc96d29ae1ad59ffa5f3f
Abdelhakim Baalla <abdelhakimbaalla40@gmail.com>
2024-09-05 13:11:29 +0100

    Fixing the logique of code

--- Day 1/Boucles L1/challenge8.c
+++ Day 1/Boucles L1/challenge8.c
@@ -3,12 +3,14 @@
 
 int main (){
     int n, f, i;
+    int x1 = 0, x2 = 1;
     printf("Saisez un nombre: ");
     scanf("%d", &n);
     for ( i = 0; i < n; i++){
-        f = f + ( n - i );
+        printf("%d ", x1);
+        f = x1 + x2;
+        x1 = x2;
+        x2 = f;
     }
-    printf("Resultat: %d", f);
-    
     return 0;
 }
