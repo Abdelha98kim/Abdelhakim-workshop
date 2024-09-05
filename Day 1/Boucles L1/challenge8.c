0e14ea40e304be0384aaeeab0e5b2b01bf5e447e
Abdelhakim Baalla <abdelhakimbaalla40@gmail.com>
2024-09-05 18:25:34 +0100

    Add comments

--- Day 1/Boucles L1/challenge8.c
+++ Day 1/Boucles L1/challenge8.c
@@ -2,10 +2,15 @@
 #include <stdlib.h>
 
 int main (){
+    // varibles
     int n, f, i;
     int x1 = 0, x2 = 1;
+
+    //demande
     printf("Saisez un nombre: ");
     scanf("%d", &n);
+
+    // make a systeme
     for ( i = 0; i <= n; i++){
         printf("%d ", x1);
         f = x1 + x2;
