29e0767624f1465e27f795de3f7cbdf101aacc96
Abdelhakim Baalla <abdelhakimbaalla40@gmail.com>
2024-09-05 09:32:34 +0100

    Fix errors and delete exe file

--- Day 2/Boucles L2/challenge2.c
+++ Day 2/Boucles L2/challenge2.c
@@ -7,7 +7,7 @@ int main (){
     printf("Saisez un nombre: ");
     scanf("%d", &nbr);
 
-    for (i = 1; i < nbr; i++){
+    for (i = 1; i <= nbr; i++){
         for ( j = 1; j <= nbr - i; j++){
             printf(" ");
         }
