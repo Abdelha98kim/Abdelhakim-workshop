aa91fde9fe9623dd80687724250b7595117d478e
Abdelhakim Baalla <abdelhakimbaalla40@gmail.com>
2024-09-05 13:01:52 +0100

    tring my idea ( the code not working good :) )

--- Day 1/Boucles L1/challenge8.c
+++ Day 1/Boucles L1/challenge8.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 
 int main (){
+    int n, f, i;
+    printf("Saisez un nombre: ");
+    scanf("%d", &n);
+    for ( i = 0; i < n; i++){
+        f = f + ( n - i );
+    }
+    printf("Resultat: %d", f);
     
     return 0;
 }
