369949456e6e6c923500140bb6521b5c094f7ae8
Abdelhakim Baalla <abdelhakimbaalla40@gmail.com>
2024-09-06 13:32:14 +0100

    Create challenge1

---
+++ Day 3/Strings/challenge1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+    char chaine[50];
+    printf("Saisez un mot: ");
+    scanf("%s", chaine);
+
+    printf("\n%s", chaine);
+    
+    return 0;
+}
