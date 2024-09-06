77352801c09c0feaa48ac533d517cad9caccd1cf
Abdelhakim Baalla <abdelhakimbaalla40@gmail.com>
2024-09-06 13:16:49 +0100

    fix a condition in for loop

--- Day 2/Les Fonctions/challenge7.c
+++ Day 2/Les Fonctions/challenge7.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+void inverse(char chaine[]);
 
 int main(){
-    char name []="hakim";
-    function(name);
-    printf("%s",name);
+    char chaine[100];
+    printf("Saisez un nom: ");
+    scanf("%s", chaine);
+    inverse(chaine);
     return 0;
+}
+
+void inverse(char chaine[]){
+    int l = strlen(chaine);
+     for (int i = 0, j = l-1; i<j; i++, j--){
+        int c = chaine[i];
+        chaine[i] = chaine[j];
+        chaine[j] = c;
+
+     }
+     printf("%s", chaine);
 }
