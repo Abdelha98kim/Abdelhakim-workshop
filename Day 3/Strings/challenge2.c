ba058de0181e49309e37aef1875615073e284abb
Abdelhakim Baalla <abdelhakimbaalla40@gmail.com>
2024-09-06 13:40:10 +0100

    create logique

--- Day 3/Strings/challenge2.c
+++ Day 3/Strings/challenge2.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main (){
-    
+    char chaine[100];
+    printf("Saisez Un mot: ");
+    scanf("%s", chaine);
+
+    int result = strlen(chaine);
+    printf("\n%d charactere", result);
     return 0;
 }
