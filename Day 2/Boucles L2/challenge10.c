44080d35730e362fe8b9da90b904590e806f50cf
Abdelhakim Baalla <abdelhakimbaalla40@gmail.com>
2024-09-10 15:22:25 +0100

    Add for loop for stocking a ramdom characters in password array and also another loop for printing the element existing in password array

--- Day 2/Boucles L2/challenge10.c
+++ Day 2/Boucles L2/challenge10.c
@@ -3,17 +3,21 @@
 #include <stdlib.h>
 
 int main(){
-    char majuscules[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
-    char miniscules[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
-    int chiffres[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    char alphabet_chiffres[] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"};
+    int size_de_tableaux = sizeof(alphabet_chiffres);
     int longueur_saisez;
     printf("Saisez la longeur de votre mots de passe: ");
     scanf("%d", &longueur_saisez);
 
-    for (int i = 0; i <= longueur_saisez; i++){
-        srand(majuscules);
-        char password = rand();
-        printf("%c", password);
+    char password[longueur_saisez];
+
+    for (int i = 0; i < longueur_saisez; i++){
+        password[i] = alphabet_chiffres[rand() % size_de_tableaux];
     }
+    
+    for (int i = 0; i < longueur_saisez; i++){
+        printf("%c", password[i]);
+    }
+    // printf("Password: %s\n", password);
     return 0;
 }
