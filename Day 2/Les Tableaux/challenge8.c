5994d09a4bd7a1d8f1913e029c6174fea6637093
Abdelhakim Baalla <abdelhakimbaalla40@gmail.com>
2024-09-10 16:31:42 +0100

    Create challenge8 and challenge 9

---
+++ Day 2/Les Tableaux/challenge8.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int tableaux_1[] = {0,1,2,3,4};
+    int size = sizeof(tableaux_1)/sizeof(tableaux_1[0]);
+    int tableaux_2[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        tableaux_2[i] = tableaux_1[i];
+    }
+
+    for (int i = 0; i < size; i++){
+        printf("%d ", tableaux_2[i]);
+    }
+    
+    return 0;
+}
