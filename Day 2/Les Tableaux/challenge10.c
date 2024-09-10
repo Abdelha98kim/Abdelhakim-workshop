ce53be41941434b2833f38f5485d67e1a0088c90
Abdelhakim Baalla <abdelhakimbaalla40@gmail.com>
2024-09-10 17:31:46 +0100

    Create Challenge 10

---
+++ Day 2/Les Tableaux/challenge10.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int la_taille_de_table;
+    printf("Saisez le nombre des elements que tu veux ajouter aux tableaux: ");
+    scanf("%d", &la_taille_de_table);
+    int tableux[la_taille_de_table];
+
+    for (int i = 0; i < la_taille_de_table; i++) {
+
+    return 0;
+}
