d6ebffe84c41ce0b1cfa8da602b919cb0e7a885f
Abdelhakim Baalla <abdelhakimbaalla40@gmail.com>
2024-09-10 15:56:45 +0100

    adding a loop in loop for comparing the element of array one by one and a loop for printing the result

--- Day 2/Les Tableaux/challenge7.c
+++ Day 2/Les Tableaux/challenge7.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    int size;
+    int size, switching;
     printf("Combien d'entier tu va stocker: ");
     scanf("%d", &size);
     int entiers[size];
@@ -10,8 +10,20 @@ int main(){
         printf("Entier %d: ", i+1);
         scanf("%d", &entiers[i]);
     }
-    // for (int i = 0; i  <= size - 1; i++){
-    //     printf("%d ", entiers[i]);
-    // }
+
+     for (int i = 0; i  <= size - 1; i++){
+         for (int j = 0; j <= size - 1; j++){
+             if(entiers[j] > entiers[j+1]){
+                switching = entiers[j];
+                entiers[j] = entiers[j+1];
+                entiers[j+1] = switching;
+             }
+            }
+            
+    }
+
+    for (int i = 0; i < size; i++){
+        printf("%d ", entiers[i]);
+    }
     return 0;
 }
